30f47487bd960e4a9b4aadd14f84183875f86b55
Shin <58554493+shinchinshin@users.noreply.github.com>
2023-08-02 00:44:19 +0700

    chore: delete dist from git

--- dist/auth/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secret = void 0;
-exports.secret = process.env.JWT_SECRET || "3d4p-secrets";
-//# sourceMappingURL=index.js.map
